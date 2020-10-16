c4246abaedb00bd6236cb89b3a39405914f51452
srpillai <srpillai@hotmail.com>
2020-10-15 19:59:23 -0500

    class 5

---
+++ exercises/class3/SchoolPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // TODO: Test your exercise solutions with print statements here.
+            Student sally = new Student("Sally", 1, 1, 4.0);
+            Console.WriteLine("The Student class works! " + sally.Name + " is a student!");
+        }
+    }
+}
